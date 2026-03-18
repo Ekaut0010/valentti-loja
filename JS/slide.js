d644604d1322cd24858a35c311332589b59194dc
Charlis <ekaut0010@gmail.com>
2026-03-18 13:44:32 -0400

    Alterações no Css e JS do Banner

--- JS/slide.js
+++ JS/slide.js
@@ -1,1 +1,23 @@
-const slides = document.querySelectorAll('.slider');
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+let index = 0;
+function showSlide(i){
+    slides.forEach(slide=>slide.classList.remove('active'));
+    dots.forEach(dot=>dot.classList.remove('active'));
+    slides[i].classList.add('active');
+    dots[i].classList.add('active');
+}
+document.querySelector('.next').onclick =()=>{
+    index = (index + 1) % slides.length;
+    showSlide(index);
+}
+document.querySelector('.prev').onclick =()=>{
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide(index);
+}
+dots.forEach((dot, i)=>{
+    dot.addEventListener('click', ()=>{
+        index = i;
+        showSlide(index);
+    });
+});
